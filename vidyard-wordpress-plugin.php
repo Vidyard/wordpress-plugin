019afbc45ccb859dd78e86fdca3a56852ea6d827
Michael Stark <mike.stark@vidyard.com>
2019-01-25 16:44:33 -0500

    update oembed to use v4 embed

--- vidyard-wordpress-plugin.php
+++ vidyard-wordpress-plugin.php
@@ -3,7 +3,7 @@
    Plugin Name: Vidyard Wordpress Plugin
    Plugin URI: http://vidyard.com
    Description: Makes it easy to embed your Vidyard videos into WordPress.
-   Version: 1.0
+   Version: 1.1
    Author: Vidyard
    Author URI: http://vidyard.com
    License: MIT
@@ -11,19 +11,19 @@
 
   wp_oembed_add_provider(
     '#https?://embed\.vidyard\.com/share/.*#i',
-    'https://api.vidyard.com/dashboard/v1/oembed',
+    'https://api.vidyard.com/dashboard/v1.1/oembed',
     true
   );
 
   wp_oembed_add_provider(
     '#https?://play\.vidyard\.com/.*#i',
-    'https://api.vidyard.com/dashboard/v1/oembed',
+    'https://api.vidyard.com/dashboard/v1.1/oembed',
     true
   );
 
   wp_oembed_add_provider(
     '#https?://.*\.hubs\.vidyard\.com/.*#i',
-    'https://api.vidyard.com/dashboard/v1/oembed',
+    'https://api.vidyard.com/dashboard/v1.1/oembed',
     true
   );
 
