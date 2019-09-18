45a49957a877b6747fd7e1322714c2fcd67385f0
Alex Pennells <alex.pennells@vidyard.com>
2019-09-18 15:20:29 -0400

    Add support for share.vidyard.com

--- vidyard-wordpress-plugin.php
+++ vidyard-wordpress-plugin.php
@@ -3,14 +3,20 @@
    Plugin Name: Vidyard Wordpress Plugin
    Plugin URI: http://vidyard.com
    Description: Makes it easy to embed your Vidyard videos into WordPress.
-   Version: 1.1
+   Version: 1.2
    Author: Vidyard
-   Author URI: http://vidyard.com
+   Author URI: https://vidyard.com
    License: MIT
    */
 
   wp_oembed_add_provider(
     '#https?://embed\.vidyard\.com/share/.*#i',
+    'https://api.vidyard.com/dashboard/v1.1/oembed',
+    true
+  );
+
+  wp_oembed_add_provider(
+    '#https?://share\.vidyard\.com/watch/.*#i',
     'https://api.vidyard.com/dashboard/v1.1/oembed',
     true
   );
